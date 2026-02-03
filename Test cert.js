3e6d7dd1666da3034557fece95d5ef2ee2e0c107
Barondev007 <135333089+Barondev007@users.noreply.github.com>
2026-02-03 11:24:21 +0100

    Refactor TLS variable and header loggingt
    Refactor TLS and header logging to output formatted strings instead of using an object. Added truncation for long header values and improved output structure.

--- Test cert.js
+++ Test cert.js
@@ -20,28 +20,50 @@ var tlsVars = [
     "client.scheme"
 ];
 
-var result = {};
+var output = "=== TLS VARIABLES ===\n";
 for (var i = 0; i < tlsVars.length; i++) {
     var varName = tlsVars[i];
     var value = context.getVariable(varName);
-    result[varName] = value || "(empty)";
+    output += varName + " = " + (value ? value : "(empty)") + "\n";
 }
 
-// Also check common headers where LBs put certs
+// Check common headers where LBs put certs
 var certHeaders = [
     "X-Client-Cert",
     "X-SSL-Client-Cert", 
     "X-SSL-Client-S-DN",
     "X-Forwarded-Client-Cert",
     "SSL_CLIENT_CERT",
-    "X-ARR-ClientCert"
+    "X-ARR-ClientCert",
+    "X-Client-Certificate",
+    "X-SSL-CERT"
 ];
 
-result["--- HEADERS ---"] = "";
+output += "\n=== CERTIFICATE HEADERS ===\n";
 for (var j = 0; j < certHeaders.length; j++) {
     var headerName = certHeaders[j];
     var headerValue = context.getVariable("request.header." + headerName);
-    result["header." + headerName] = headerValue ? headerValue.substring(0, 100) + "..." : "(empty)";
+    if (headerValue) {
+        // Truncate long values
+        var displayValue = headerValue.length > 100 ? headerValue.substring(0, 100) + "..." : headerValue;
+        output += headerName + " = " + displayValue + "\n";
+    } else {
+        output += headerName + " = (empty)\n";
+    }
 }
 
-context.setVariable("debug.tls.info", JSON.stringify(result, null, 2));
+// List all request headers to find any cert-related ones
+output += "\n=== ALL REQUEST HEADERS ===\n";
+var headerNames = context.getVariable("request.headers.names");
+if (headerNames) {
+    var headers = String(headerNames).replace(/[\[\]]/g, "").split(", ");
+    for (var k = 0; k < headers.length; k++) {
+        var hName = headers[k].trim();
+        if (hName) {
+            var hValue = context.getVariable("request.header." + hName);
+            output += hName + " = " + (hValue ? hValue.substring(0, 80) : "(empty)") + "\n";
+        }
+    }
+}
+
+context.setVariable("debug.tls.info", output);
